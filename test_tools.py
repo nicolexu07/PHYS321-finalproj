44d521a8148b6320d03246930b7d34184a9754d8
William Pugsley <WillPugs@hotmail.ca>
2022-03-30 11:12:36 -0400

    Modified functions that load data to be more general

--- test_tools.py
+++ test_tools.py
@@ -2,8 +2,8 @@ import unittest
 import tools
 import nose.tools as nt
 import numpy as np
-
-class test_tools(unittest.TestCase):
+import pandas as pd
+class test_tools_functions(unittest.TestCase):
     def test_solve_for_u(self):
         e = np.random.uniform(0, 1)
         T = np.random.uniform(3282.3503, 3.46896e13)
@@ -19,3 +19,12 @@ class test_tools(unittest.TestCase):
         temp = solve_for_u(t, tau, T, e)
         #when e=0 the function is linear
         self.assertAlmostEqual(2*np.pi*(t-tau)/T, temp)
+
+    def test_get_data(self):
+        temp = tools.get_data('test_data.tbl', '')
+        self.assertEqual(temp[0], pd.Series([0, 86400]))
+
+
+if __name__ == '__main__':
+    #unittest.main()
+    None
