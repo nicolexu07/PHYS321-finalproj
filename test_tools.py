f5bcb2647b6c158c0ce33be67707379369a4790f
Nicole Xu <nicolexu07@gmail.com>
2022-04-04 23:50:38 -0400

    Revert "Merge branch 'main' into create_simulation"
    This reverts commit acd4ac5ccac03fa3c459ed33e6d2479cd33af1f6, reversing
    changes made to bc550b975421b581ceb7271eadfb5844efaf3319.

--- test_tools.py
+++ test_tools.py
@@ -3,31 +3,28 @@ import tools
 import nose.tools as nt
 import numpy as np
 import pandas as pd
-
-
-
-class TestToolsFunctions():
+class test_tools_functions(unittest.TestCase):
     def test_solve_for_u(self):
         e = np.random.uniform(0, 1)
         T = np.random.uniform(3282.3503, 3.46896e13)
-        tau = np.random.uniform(3282.3503, 3.46896e13)
+        tau = n.random.uniform(3282.3503, 3.46896e13)
 
         #when t=tau we expect u=0
         #numerical solver gives approximately 0
-        temp = tools.solve_for_u(tau, tau, T, e)
-        nt.assert_almost_equal(0, temp)
+        temp = solve_for_u(tau, tau, T, e)
+        self.assertAlmostEqual(0, temp)
 
         e = 0
         t = np.random.uniform(0, T)
-        temp = tools.solve_for_u(t, tau, T, e)
+        temp = solve_for_u(t, tau, T, e)
         #when e=0 the function is linear
-        nt.assert_almost_equal(2*np.pi*(t-tau)/T, temp)
-    
+        self.assertAlmostEqual(2*np.pi*(t-tau)/T, temp)
+
     def test_get_data(self):
         temp = tools.get_data('test_data.tbl', '')
-        nt.assert_equal(temp[0], pd.Series([0, 86400]))
+        self.assertEqual(temp[0], pd.Series([0, 86400]))
 
 
-    def test_find_files_for_star(self):
-        nt.assert_raises(ValueError, tools.find_files_for_star, 'CrazyFrog')
-
+if __name__ == '__main__':
+    #unittest.main()
+    None
